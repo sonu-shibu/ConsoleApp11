0435e6814af25cf41492b5ec82e2dbd720a9baa2
sonu.inmca2126 <sonu.inmca2126@OPENSOURCELAB03>
2024-04-04 10:27:27 +0530

    Add project files.

---
+++ ConsoleApp11/Program.vb
@@ -0,0 +1,38 @@
+Imports System
+Public Class Rectangle
+    Private length As Integer
+    Private width As Integer
+
+    Public Sub New(ByVal length As Integer, ByVal width As Integer)
+        Me.length = length
+        Me.width = width
+    End Sub
+
+    Public Function CalculateArea() As Integer
+        Return length * width
+    End Function
+
+    Public Function CalculatePerimeter() As Integer
+        Return 2 * (length + width)
+    End Function
+End Class
+
+Module Program
+    Sub Main()
+
+        Dim rectangle1 As New Rectangle(5, 3)
+
+        Console.WriteLine("Rectangle 1:")
+        Console.WriteLine("Area: " & rectangle1.CalculateArea())
+        Console.WriteLine("Perimeter: " & rectangle1.CalculatePerimeter())
+
+        Dim rectangle2 As New Rectangle(7, 4)
+
+        Console.WriteLine(vbCrLf & "Rectangle 2:")
+        Console.WriteLine("Area: " & rectangle2.CalculateArea())
+        Console.WriteLine("Perimeter: " & rectangle2.CalculatePerimeter())
+
+        Console.ReadLine()
+    End Sub
+End Module
+
